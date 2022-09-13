26cb4bbcc3eed38d404337ff9c0de103fbbdf861
Quan Nguyen Tu <qnt001@bucknell.edu>
2022-09-13 08:06:11 -0400

    lsp ccls config

--- init.lua
+++ init.lua
@@ -14,9 +14,11 @@ return require('packer').startup(function(use)
   -- add you plugins here like:
   --------------nvim-cmp------------------
   use 'neovim/nvim-lspconfig'
+  use {"williamboman/nvim-lsp-installer",
+  require("nvim-lsp-installer").setup {}}
   use "rafamadriz/friendly-snippets"
   use 'L3MON4D3/LuaSnip'
-  use { 'saadparwaiz1/cmp_luasnip' }
+  use 'saadparwaiz1/cmp_luasnip'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
@@ -56,108 +58,74 @@ return require('packer').startup(function(use)
     end
 
 
---[[-------cmp setup --------
-local cmp = require('cmp')
-local luasnip = require('luasnip')
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+-------cmp setup --------
+local cmp = require'cmp'
 
-require('luasnip.loaders.from_vscode').lazy_load()
-vim.api.nvim_command('hi LuasnipChoiceNodePassive cterm=italic')
-
-vim.opt.completeopt = 'menu,menuone,noselect'
-
-cmp.setup({
+  cmp.setup({
     snippet = {
-        expand = function(args)
-           -- require('luasnip').lsp_expand(args.body)
-        end,
-    },
-    formatting = {
-        format = function(entry, item)
-            local menu_map = {
-                buffer = '[Buf]',
-                nvim_lsp = '[LSP]',
-                nvim_lua = '[API]',
-                path = '[Path]',
-                luasnip = '[Snip]',
-                rg = '[RG]',
-            }
-
-            item.menu = menu_map[entry.source.name] or string.format('[%s]', entry.source.name)
-            item.kind = vim.lsp.protocol.CompletionItemKind[item.kind]
-
-            return item
-        end,
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
     },
     window = {
-        completion = {
-            border = 'rounded',
-            winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None',
-        },
-        documentation = {
-            border = 'rounded',
-            winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None',
-        },
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4)),
-        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4)),
-        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete()),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<C-n>'] = {
-            c = function(fallback)
-                local cmp = require('cmp')
-                if cmp.visible() then
-                    cmp.select_next_item()
-                else
-                    fallback()
-                end
-            end,
-        },
-        ['<C-p>'] = {
-            c = function(fallback)
-                local cmp = require('cmp')
-                if cmp.visible() then
-                    cmp.select_prev_item()
-                else
-                    fallback()
-                end
-            end,
-        },
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end,
-        ['<S-Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end,
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
-        { name = 'nvim_lsp', priority = 10 },
-        { name = 'nvim_lsp_signature_help' },
-        { name = 'luasnip' },
+      { name = 'nvim_lsp' },
+      -- { name = 'vsnip' }, -- For vsnip users.
+      { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
     }, {
-        { name = 'nvim_lua' },
-    }, {
-        { name = 'buffer' },
-        { name = 'path' },
-        { name = 'rg' },
-    }),
-})
+      { name = 'buffer' },
+    })
+  })
 
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+  -- Set up lspconfig.
+  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+   require('lspconfig')['ccls'].setup {
+    capabilities = capabilities
+  }
 -- https://github.com/windwp/nvim-autopairs
-require('nvim-autopairs').setup()--]]
+require('nvim-autopairs').setup()
   end
 )
