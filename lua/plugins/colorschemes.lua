e8741b66300dbfd821ac0eed42a5fc505bd93695
Quan Nguyen Tu <qnt001@bucknell.edu>
2026-08-21 00:31:08 -0400

    change colorscheme to everforest

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,12 +1,19 @@
+local active_theme = "everforest" --WIP
+
+local use_theme = function(name)
+	local fine, err = pcall(vim.cmd.colorscheme, name)
+	if not fine then
+		vim.notify(string.format("Failed to load colorscheme %s, err: %s", name, err),
+			vim.log.levels.WARN)
+	end
+end
+
 return {
 	--tokyonight--
-	{"folke/tokyonight.nvim",
-  	lazy = false,
-  	priority = 1000,
-  	opts = {},
+	{"folke/tokyonight.nvim",lazy = false, priority = 1000},
+	{"sainnhe/everforest", lazy = false, priority = 1000,
 	config = function ()
-		vim.cmd.colorscheme("tokyonight-night")
-		
-	end,
-	},
+		use_theme("everforest")
+	end},
+
 }
